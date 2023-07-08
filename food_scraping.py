c39c7e18a4ef38b5657c8bb0e365c280e3e535e0
ik04 <ishaankhurana123@gmail.com>
2023-07-09 00:31:44 +0530

    improve code to get a more desctructurable output

--- food_scraping.py
+++ food_scraping.py
@@ -1,29 +1,27 @@
 from bs4 import BeautifulSoup as bs
 import requests
-import pandas as pd
+import json
 
 text1 = requests.get("https://www.dominos.co.in/menu/veg-pizzas")
 
 soup = bs(text1.text, "html.parser")
 row = soup.find_all(class_="row")
-pizza_names = []
-des = []
+
+pizza_list = []
+
 for i in row:
     name = i.find("h3")
     final = name.a.text
-    pizza_names.append(final)
 
-print(pizza_names)
+    desc = i.find("p").get_text()
 
-for x in row:
-    desc = x.find("p").get_text()
-    finakl = desc
-    des.append(desc)
+    pizza = {"pizza_name": final, "description": desc}
+    pizza_list.append(pizza)
 
-
-our_data = pd.DataFrame({"Pizza Names": pizza_names, "Description": des})
-json_output = our_data.to_json()
+output_dict = {"items": pizza_list}
+json_output = json.dumps(output_dict, indent=4)
 
 print(json_output)
+
 with open("./output.json", "w") as fs:
     fs.write(json_output)
